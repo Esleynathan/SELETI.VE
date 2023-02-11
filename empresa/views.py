6f48a7aa6a190b6ee41deaa5b99207efc4a7754b
Esley <esleynathan@hotmail.com>
2023-02-11 01:10:52 -0300

    SELETI.VE | Concluindo 'Criar Nova Empresa'

--- empresa/views.py
+++ empresa/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Tecnologias
+from .models import Tecnologias, Empresa
+from django.shortcuts import redirect
+from django.contrib.messages import constants
+from django.contrib import messages
+
 
 def nova_empresa(request):
     if request.method == "GET":
@@ -12,6 +16,31 @@ def nova_empresa(request):
         cidade = request.POST.get('cidade')
         endereco = request.POST.get('endereco')
         nicho = request.POST.get('nicho')
-        caracteriscas = request.POST.get('caracteristicas')
+        caracteriscas = request.POST.get('caracteristicas')        
+        tecnologias = request.POST.getlist('tecnologias')
+        logo = request.FILES.get('logo')
+
+        if (len(nome.strip()) == 0 or len(email.strip()) == 0 or len(cidade.strip()) == 0 or len(endereco.strip()) == 0 or len(nicho.strip()) == 0 or len(caracteristicas.strip()) == 0 or (not logo)): 
+            messages.add_message(request, constants.ERROR, 'Preencha todos os campos')
+            return redirect('/home/nova_empresa')
+
+        if logo.size > 100_000_000:
+            messages.add_message(request, constants.ERROR, 'A logo da empresa deve ter menos de 10MB')
+            return redirect('/home/nova_empresa')
+
+        if nicho not in [i[0] for i in Empresa.choices_nicho_mercado]:
+            messages.add_message(request, constants.ERROR, 'Nicho de mercado inválido')
+            return redirect('/home/nova_empresa')
         
-        return HttpResponse("Tô aqui")
+        empresa = Empresa(logo=logo,
+                        nome=nome,
+                        email=email,
+                        cidade=cidade,
+                        endereco=endereco,
+                        nicho_mercado=nicho,
+                        caracteristica_empresa=caracteristicas)
+        empresa.save()
+        empresa.tecnologias.add(*tecnologias)
+        empresa.save()
+        messages.add_message(request, constants.SUCCESS, 'Empresa cadastrada com sucesso')
+        return redirect('/home/empresas')
